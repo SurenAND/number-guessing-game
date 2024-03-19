be650eea2c472da10be0fd5bbd1b6b63861932ad
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-19 14:14:10 +0330

    get try again button

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 let input = document.querySelector("input");
 let guess = document.querySelector(".guess");
 let tryBtn = document.querySelector(".try-btn");
+let playAgainBtn = document.querySelector(".play-again-btn");
